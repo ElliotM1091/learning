f0dff9ce6bb66b14dbfc99d5113672cd44c99f7e
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-06 10:40:35 -0600

    added a gitignore for training

---
+++ leanring.php
@@ -0,0 +1,15 @@
+<?php
+$user_name = $_REQUEST['username'];
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Page Title</title>
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		Welcome <?php echo $username;?>
+	</body>
+</html>
